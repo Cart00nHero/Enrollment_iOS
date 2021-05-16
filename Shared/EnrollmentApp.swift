2efd5037f9a3e0421a11a41e983f5d879a11fe30
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-16 17:59:42 +0800

    Initial Commit

---
+++ Shared/EnrollmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnrollmentApp.swift
+//  Shared
+//
+//  Created by YuCheng on 2021/5/16.
+//
+
+import SwiftUI
+
+@main
+struct EnrollmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
