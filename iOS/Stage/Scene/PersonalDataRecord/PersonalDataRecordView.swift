ac63bba3e97d891e4c4629e439bdb9182245136b
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-16 19:31:03 +0800

    init scenario

---
+++ iOS/Stage/Scene/PersonalDataRecord/PersonalDataRecordView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalDataRecordView.swift
+//  Enrollment (iOS)
+//
+//  Created by YuCheng on 2021/5/16.
+//
+
+import SwiftUI
+
+struct PersonalDataRecordView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PersonalDataRecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalDataRecordView()
+    }
+}
