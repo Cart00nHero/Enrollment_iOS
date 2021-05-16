3fa1dcdf53af87bcd62b2b623e0ea442c4553f0a
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-16 19:06:00 +0800

    Init project

---
+++ iOS/Stage/Scene/UserInfoInputView.swift
@@ -0,0 +1,20 @@
+//
+//  UserInfoInputView.swift
+//  Enrollment (iOS)
+//
+//  Created by YuCheng on 2021/5/16.
+//
+
+import SwiftUI
+
+struct UserInfoInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserInfoInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserInfoInputView()
+    }
+}
