efc59aab856ca65cca83bf2f1e91fe9874668249
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-23 14:05:00 +0800

    Update start view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -20,17 +20,30 @@ struct ContentView: View {
                     isActive: $activeRoleSelection) {
                     Text("")
                 }.hidden()
+                .frame(width: 0.0, height: 0.0, alignment: .center)
                 NavigationLink(
                     destination: iOSTabView(),
                     isActive: $activeVisitorUI) {
                     Text("")
                 }.hidden()
+                .frame(width: 0.0, height: 0.0, alignment: .center)
                 NavigationLink(
                     destination: iOSBrowserView(),
                     isActive: $activeBrowser) {
                     Text("")
+                }.hidden()
+                .frame(width: 0.0, height: 0.0, alignment: .center)
+                ZStack(alignment: Alignment(horizontal: .center, vertical: .center)) {
+                    Image("image_space_surise").resizable().aspectRatio(contentMode: .fill)
+                    Text("Even after the darkest nights, morning always comes.")
+                        .foregroundColor(Color.white)
+                        .font(Font.system(size: 18.0))
+                        .multilineTextAlignment(.center)
                 }
-            }.onAppear() {
+            }.navigationBarHidden(true)
+            .preferredColorScheme(.dark)
+            .foregroundColor(.black)
+            .onAppear() {
                 // 頁面切太快會有奇怪的事情發生
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                    // Code you want to be delayed
