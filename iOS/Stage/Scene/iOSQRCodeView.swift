05693e06a63f4f093154b6832187c18bb935684c
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-27 14:54:47 +0800

    Add qurCode scenario

--- iOS/Stage/Scene/iOSQRCodeView.swift
+++ iOS/Stage/Scene/iOSQRCodeView.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+fileprivate let scenario: QRCodeScenario = QRCodeScenario()
 struct iOSQRCodeView: View {
+    @State private var presentPhotoLibrary = false
+    @State private var selectedImage: UIImage = UIImage()
+    @State private var toggleSwitchOn = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Toggle(isOn: $toggleSwitchOn) {
+                Text("")
+            }.onChange(of: toggleSwitchOn) { isOn in
+                if isOn {
+                    print("送")
+                } else {
+                    print("停")
+                }
+            }
+            Image(uiImage: selectedImage)
+                .resizable().scaledToFit()
+            Divider()
+            Button {
+                presentPhotoLibrary = true
+            } label: {
+                Text("選取QRCode圖片")
+                    .foregroundColor(flameScarlet(1.0))
+            }
+        }.onAppear() {
+            scenario.beCollectParcel()
+        }
+        .sheet(isPresented: $presentPhotoLibrary) {
+            SwiftUIPhotoPicker(
+                sourceType: .photoLibrary, selectedImage: $selectedImage)
+        }
     }
 }
 
