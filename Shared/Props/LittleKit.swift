2362e210fae79afe0d74a3843907eca3edca387a
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-06-05 15:54:08 +0800

    Update

--- Shared/Props/LittleKit.swift
+++ Shared/Props/LittleKit.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
 
 func localized(_ key: String) -> String {
     return NSLocalizedString(key, comment: "")
 }
+func autoUISize(_ value: CGFloat) -> CGFloat {
+    return value * UIScreen.main.bounds.width/375.0
+}
+func autoFont(value: CGFloat) -> CGFloat {
+    if UIScreen.main.bounds.width < 375.0 {
+        return (value - 2.0)
+    }
+    if UIScreen.main.bounds.width > 375.0 {
+        return (value + 2.0)
+    }
+    return value
+}
+
+let screenSize = UIScreen.main.bounds.size
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
--- Shared/Props/SingletonStorage.swift
+++ Shared/Props/SingletonStorage.swift
@@ -14,15 +14,3 @@ class SingletonStorage: NSObject {
 }
 
 let serviceType = "visitor-record"
-func autoUISize(_ value: CGFloat) -> CGFloat {
-    return value * UIScreen.main.bounds.width/375.0
-}
-func autoFont(value: CGFloat) -> CGFloat {
-    if UIScreen.main.bounds.width < 375.0 {
-        return (value - 2.0)
-    }
-    if UIScreen.main.bounds.width > 375.0 {
-        return (value + 2.0)
-    }
-    return value
-}
